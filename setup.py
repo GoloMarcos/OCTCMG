4f9841482a0fb6b07e1dd1fac915445bc6c7e2ca
Marcos Paulo Silva Gôlo <golo@pink.labic.icmc.usp.br>
2021-04-05 16:53:51 -0300

    add files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup ( 
+    name = 'OneClassTextCollectionsLibrary', 
+    packages = find_packages(), 
+    version = '0.1.0', 
+    description = 'Library to use one-class text collections in which the texts are news from GEDELT project and each collection are based on the taxonomy of IPTC', 
+    author = 'Marcos P. S. Gôlo',  
+    install_requires = ['gdown']
+)
