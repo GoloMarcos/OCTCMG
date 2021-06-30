3c32fd10c6730a764033214cb2e49e5e25d61dfb
Marcos Gôlo <68027659+GoloMarcos@users.noreply.github.com>
2021-06-30 11:21:59 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup ( 
     name = 'OneClassTextCollectionsLibrary', 
     packages = find_packages(), 
-    version = '0.1.0', 
+    version = '0.2.0', 
     description = 'Library to use one-class text collections in which the texts are news from GEDELT project and each collection are based on the taxonomy of IPTC', 
     author = 'Marcos P. S. Gôlo',  
-    install_requires = ['gdown']
+    install_requires = ['gdown', 'pandas']
 )
