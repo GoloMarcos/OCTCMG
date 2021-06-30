4c54b95141dbe02a57f3b274661882d0a08ef2b5
Marcos Gôlo <68027659+GoloMarcos@users.noreply.github.com>
2021-06-30 10:39:00 -0300

    add only_download and dataset parameters (v0.2)

--- OneClassTextCollectionsLibrary/datasets.py
+++ OneClassTextCollectionsLibrary/datasets.py
@@ -2,30 +2,36 @@ import pandas as pd
 import os
 from pathlib import Path
 
-def load(path_files=''):
+def load(path_files='', only_download = False, dataset = 'All'):
 
   '''
   this function download and load all datasets, and return a dictionary in which any key represent the dataset
   
   if the directory already exist, to load them, pass the path where the directory are through the path_files parameter
   
-  this function changes the current directory, remember to go back to the directory you were in
   '''
 
   if path_files != '':
     os.chdir(path_files)
 
-  if not Path('News.zip').is_file():
-    os.system('gdown --id 1-20ozBt8NjnN1X_F_HkXVMlDl67CUYNW')
-    os.system('unzip News.zip') 
+  if not Path(path_files + '/News.zip').is_file():
+    cmd = 'gdown --id 1-20ozBt8NjnN1X_F_HkXVMlDl67CUYNW -O ' + path_files + '/News.zip'
+    os.system(cmd)
+    cmd2 = 'unzip ' + path_files + '/News.zip'
+    os.system(cmd2) 
+    
+  if not only_download:
+    if dataset == 'All':
+      basepath = Path(path_files + '/News/')
+      files_in_basepath = basepath.iterdir()
+      datasets = {}
+      for item in files_in_basepath:
+        if item.is_file():
+          df = pd.read_pickle(path_files + '/News/' + item.name)
+          datasets[item.name.replace('.plk','')] = df
+    
+    else:
+      datasets = {}
+      datasets[dataset] = pd.read_pickle(path_files + '/News/' + dataset + '.plk')
 
-  
-  basepath = Path('./News/')
-  files_in_basepath = basepath.iterdir()
-  datasets = {}
-  for item in files_in_basepath:
-    if item.is_file():
-      df = pd.read_pickle('./News/' + item.name)
-      datasets[item.name.replace('.plk','')] = df
-
-  return datasets
+    return datasets
