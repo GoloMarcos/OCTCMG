40ffd689e3fa50197c8b038c7fe11495c1b982be
Marcos Gôlo <marcosgolo@usp.br>
2025-05-05 16:11:31 +0200

    add readthedocs files conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,11 +14,16 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+extensions = ['myst_parser']
+
 source_suffix = {
     '.rst': 'restructuredtext',
     '.txt': 'markdown',
     '.md': 'markdown',
 }
+
+templates_path = ['_templates']
+exclude_patterns = []
 
 language = 'en'
 
